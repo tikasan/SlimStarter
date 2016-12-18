6aacaecab1ed71291d3d458ebf193fd14560f806
tikasan <satak47cpc@gmail.com>
2016-12-18 16:05:30 +0900

    add: ルーティング作成＋viewの作成

--- app/controllers/RecipeController.php
+++ app/controllers/RecipeController.php
@@ -3,9 +3,57 @@
 Class RecipeController extends BaseController
 {
 
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    // getでrecipe/にアクセスされた場合
+    // 検索画面
     public function index()
     {
-        $this->data['title'] = 'Recipe';
-        App::render('index.twig', $this->data);
+
+    }
+
+    // getでrecipe/createにアクセスされた場合
+    // 入力画面と確認画面はここでする。
+    public function create()
+    {
+
+    }
+
+    // postでrecipe/にアクセスされた場合
+    // 新規作成処理
+    public function store()
+    {
+
+    }
+
+    // getでrecipe/:idにアクセスされた場合
+    // 詳細画面
+    public function show($id)
+    {
+
+    }
+
+    // getでrecipe/:id/editにアクセスされた場合
+    // 編集画面
+    public function edit($id)
+    {
+
+    }
+
+    // putまたはpatchでrecipe/:idにアクセスされた場合
+    // 更新処理
+    public function update($id)
+    {
+
+    }
+
+    // deleteでrecipe/:idにアクセスされた場合
+    // 削除処理
+    public function destroy($id)
+    {
+
     }
 }
